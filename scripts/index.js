f34dd079a031b973d7ca0e8a646e767c252ca3aa
Denis Shesternev <denshesternev@yandex.ru>
2023-02-11 17:59:38 +0800

    fix: исправление ошибок 1-го ревью

--- scripts/index.js
+++ scripts/index.js
@@ -1,26 +1,31 @@
-let openPopup = document.querySelector('.profile__edit-button');
+let profileEdit = document.querySelector('.profile__edit-button');
 let closePopup = document.querySelector('.popup__close-button');
 let popup = document.querySelector('.popup')
 let formElement = document.querySelector('.popup__form');
-let nameInput = document.querySelector('.popup__input-name');
-let jobInput = document.querySelector('.popup__input-add');
+let nameInput = document.querySelector('.popup__input_name');
+let jobInput = document.querySelector('.popup__input_add');
 let profileName = document.querySelector('.profile__name');
 let profileAdd = document.querySelector('.profile__add');
 
+// функция равенства полей на странице и в popup
+
+function initializePopup() {
+    nameInput.value = profileName.textContent;
+    jobInput.value = profileAdd.textContent;
+}
+initializePopup();
 
 // функция открытия popup
 
 function open() {
-    popup.classList.add('popup__open');
-    nameInput.value = profileName.textContent;
-    jobInput.value = profileAdd.textContent;
+    popup.classList.add('popup__opened');
 }
-openPopup.addEventListener('click', open);
+profileEdit.addEventListener('click', open);
 
 // функция закрытия popup
 
 function close() {
-    popup.classList.remove('popup__open');
+    popup.classList.remove('popup__opened');
 }
 closePopup.addEventListener('click', close);
 
