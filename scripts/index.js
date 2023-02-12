61836aac1dd46e9d13ae34c517721806596b548e
Denis Shesternev <denshesternev@yandex.ru>
2023-02-12 11:30:28 +0800

    fix: исправление ошибок 2-го ревью

--- scripts/index.js
+++ scripts/index.js
@@ -2,25 +2,23 @@ let profileEdit = document.querySelector('.profile__edit-button');
 let closePopup = document.querySelector('.popup__close-button');
 let popup = document.querySelector('.popup')
 let formElement = document.querySelector('.popup__form');
-let nameInput = document.querySelector('.popup__input_name');
-let jobInput = document.querySelector('.popup__input_add');
+let nameInput = document.querySelector('.popup__input_data_name');
+let jobInput = document.querySelector('.popup__input_data_add');
 let profileName = document.querySelector('.profile__name');
 let profileAdd = document.querySelector('.profile__add');
-
-// функция равенства полей на странице и в popup
-
-function initializePopup() {
-    nameInput.value = profileName.textContent;
-    jobInput.value = profileAdd.textContent;
-}
-initializePopup();
 
 // функция открытия popup
 
 function open() {
     popup.classList.add('popup__opened');
 }
-profileEdit.addEventListener('click', open);
+
+profileEdit.addEventListener('click', function() {
+    open();
+    nameInput.value = profileName.textContent;
+    jobInput.value = profileAdd.textContent;
+}
+);
 
 // функция закрытия popup
 
