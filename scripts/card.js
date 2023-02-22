e4e54072b28b53e61e7ee39528a1e89297d08063
Denis Shesternev <denshesternev@yandex.ru>
2023-02-22 12:31:13 +0800

    refactor: исправления после 1-го ревью

--- scripts/card.js
+++ scripts/card.js
@@ -24,28 +24,4 @@ const initialCards = [
       name: 'Байкал',
       link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
     },
-    {
-        name: 'Алтай',  // добавил карточек через относительный путь в папке.
-        link: '../images/1.jpg'
-    },
-    {
-        name: 'Пик Тальцинский', 
-        link: '../images/2.jpg'
-    },
-    {
-        name: 'Байкал',
-        link: '../images/3.jpg'
-    },
-    {
-        name: 'Пик Хулугайша',
-        link: '../images/4.jpg'
-    },
-    {
-        name: 'Гранатовый маршрут',
-        link: '../images/5.jpg'
-    },
-    {
-        name: 'Мамай',
-        link: '../images/6.jpg'
-    }
   ];
